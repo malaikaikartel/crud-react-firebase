dcb854b9f809f755697630cb826950fc9bf5b33a
malaikaikartel <50528583+malaikaikartel@users.noreply.github.com>
2023-06-04 20:27:00 +0700

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,6 +4,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 var firebaseConfig = {
   // Paste Your keys here
+  apiKey: "AIzaSyBy1fxtGHrXVFlHHYUVafny5SHV8rVCs6c",
+  authDomain: "redframe-77d5a.firebaseapp.com",
+  projectId: "redframe-77d5a",
+  storageBucket: "redframe-77d5a.appspot.com",
+  messagingSenderId: "806740627891",
+  appId: "1:806740627891:web:7025d1386396f9cdb3fbc6",
+  measurementId: "G-HSL5EMRL9W"
 };
 // Initialize Firebase
 initializeApp(firebaseConfig);
